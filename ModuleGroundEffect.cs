1b4a48e0cd4f160ea72f657fb48f3cf19b463f20
Neal_Nicdao <chrisnicdao0@gmail.com>
2018-10-08 21:20:17 -0700

    Added code for control surfaces
     * Some optimizations that determine when the force should be active
     * A few other changes

--- ModuleGroundEffect.cs
+++ ModuleGroundEffect.cs
@@ -7,34 +7,48 @@ namespace KSP_GroundEffect
 	[KSPAddon(KSPAddon.Startup.Flight, false)]
 	public class ModuleGroundEffect : PartModule 
 	{ 
-		ModuleLiftingSurface thingThatLiftsParts;
-		float initialLift;
 
+		const float ActivateAltitude = 400;
+
+		ModuleLiftingSurface thingThatLiftsParts;
+		ModuleControlSurface thingThatAlsoLiftsPartsButMoves;
+		float initialLift;
+		float initialLiftCtrl;
 		float wingSpan = 4.5f; // strange assumption m8
 
 		public override void OnStart(StartState state) {
 			// Look through the list of part modules to find anything that inherits ModuleLiftingSurface
 			foreach (PartModule module in part.Modules) {
-				if (module is ModuleLiftingSurface) {
+				if (typeof(ModuleLiftingSurface).IsAssignableFrom(module.GetType())) {
 					thingThatLiftsParts = (ModuleLiftingSurface)(module);
 					initialLift = thingThatLiftsParts.deflectionLiftCoeff;
-					print("Ground effect module loaded");
+					if (module is ModuleControlSurface) {
+						//print ("SURFAAAACE!");
+						thingThatAlsoLiftsPartsButMoves = (ModuleControlSurface)(module);
+						initialLiftCtrl = thingThatAlsoLiftsPartsButMoves.ctrlSurfaceArea;
+					}
+					print ("Ground effect module loaded");
 					return;
 				}
 			}
+			// This means there's a module manager error?
 			print("Ground effect module loaded for non-aero part?");
-
 		}
 
 		public override void OnUpdate() {
-			
-			if (!(vessel.situation == Vessel.Situations.FLYING) || !vessel.mainBody.hasSolidSurface || !vessel.mainBody.atmosphere) {
+
+			// Checks to see if ground effect would have any significance
+			if (thingThatLiftsParts == null
+				|| ((vessel.situation & (Vessel.Situations.FLYING | Vessel.Situations.LANDED | Vessel.Situations.SPLASHED)) == 0)
+				|| (vessel.radarAltitude > ActivateAltitude)
+				|| !vessel.mainBody.hasSolidSurface
+				|| !vessel.mainBody.atmosphere) {
+				// End immediately if it doesnt
 				return;
 			}
 
-			thingThatLiftsParts.deflectionLiftCoeff = initialLift;
-
 			float groundDistance = 0;
+			float multiplier = 0;
 
 			// Distance from ocean,
 			if (FlightGlobals.currentMainBody.ocean) {
@@ -49,10 +63,20 @@ namespace KSP_GroundEffect
 			}
 
 			if (groundDistance != 0) {
-				thingThatLiftsParts.deflectionLiftCoeff *= 2.0f / (float)Math.Pow (0.3f * groundDistance + 1.0f, 2) + 1.0f;
+				multiplier = 2.0f / (float)Math.Pow (0.3f * groundDistance + 1.0f, 2) + 1.0f;
 			}
 
-			//print ("Lift " + thingThatLiftsParts.deflectionLiftCoeff);
+			//print ("Lift Multiplier " + multiplier + " alt: " + vessel.radarAltitude);
+
+
+			//((thingThatAlsoLiftsPartsButMoves == null) ? thingThatLiftsParts.deflectionLiftCoeff : thingThatAlsoLiftsPartsButMoves.ctrlSurfaceArea) = nextLift;
+			thingThatLiftsParts.deflectionLiftCoeff = initialLift * multiplier;
+
+			if (thingThatAlsoLiftsPartsButMoves != null) {
+				//print ("CTRL SURFACE!!");
+				thingThatAlsoLiftsPartsButMoves.ctrlSurfaceArea = initialLiftCtrl * multiplier;
+			}
+
 		}
 
 	}
