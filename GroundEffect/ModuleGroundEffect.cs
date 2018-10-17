2e64192d6268096349abc6e05513ff9eca0d290b
Neal_Nicdao <chrisnicdao0@gmail.com>
2018-10-16 17:54:35 -0700

    Fixed things and organized

--- GroundEffect/ModuleGroundEffect.cs
+++ GroundEffect/ModuleGroundEffect.cs
@@ -37,37 +37,48 @@ namespace KSP_GroundEffect
 
 		public override void OnUpdate() {
 
-			// Checks to see if ground effect would have any significance
-			if (thingThatLiftsParts == null
-				|| ((vessel.situation & (Vessel.Situations.FLYING | Vessel.Situations.LANDED | Vessel.Situations.SPLASHED)) == 0)
-				|| (vessel.radarAltitude > ActivateAltitude)
-				|| !vessel.mainBody.hasSolidSurface
-				|| !vessel.mainBody.atmosphere) {
-				// End immediately if it doesnt
-				return;
-			}
-
-			float groundDistance = 0;
 			float multiplier = 0;
 
-			// Distance from ocean,
-			if (FlightGlobals.currentMainBody.ocean) {
-				groundDistance = Math.Max(FlightGlobals.getAltitudeAtPos (part.transform.position), 0);
-			}
+			// Checks to see if ground effect would have any significance
+			if (thingThatLiftsParts == null
+			    || ((vessel.situation & (Vessel.Situations.FLYING | Vessel.Situations.LANDED | Vessel.Situations.SPLASHED)) == 0)
+			    || (vessel.radarAltitude > ActivateAltitude)
+			    || !vessel.mainBody.hasSolidSurface
+			    || !vessel.mainBody.atmosphere) {
+				// End immediately if it doesnt
+				//return;
 
-			// Test for terrain
-			RaycastHit ray;
-			if (Physics.Raycast (part.transform.position, vessel.gravityForPos.normalized, out ray, wingSpan * 2, 1 << 15)) {
-				// Close to ground, override groundDistance if close to terrain, buildings, or anything
-				groundDistance = (groundDistance == 0) ? ray.distance : Math.Min(groundDistance, ray.distance);
-			}
+				// nevermind, just set multiplier to 1
+				// so that coefficients are set back to default values
+				multiplier = 1.0f;
+			} else {
+				
+				float groundDistance = 0;
+				Vector3 surfaceNormal = vessel.gravityForPos.normalized;
 
-			if (groundDistance != 0) {
-				multiplier = 2.0f / (float)Math.Pow (0.3f * groundDistance + 1.0f, 2) + 1.0f;
+				// Distance from ocean,
+				if (FlightGlobals.currentMainBody.ocean) {
+					groundDistance = Math.Max(FlightGlobals.getAltitudeAtPos (part.transform.position), 0);
+				}
+
+				// Test for terrain
+				RaycastHit ray;
+				if (Physics.Raycast (part.transform.position, surfaceNormal, out ray, wingSpan * 2, 1 << 15)) {
+					// Close to ground, override groundDistance if close to terrain, buildings, or anything
+					groundDistance = (groundDistance == 0) ? ray.distance : Math.Min (groundDistance, ray.distance);
+
+					// also set surface normal
+					surfaceNormal = ray.normal;
+				}
+				if (groundDistance != 0) {
+					//print ("Wing up: " + thingThatLiftsParts.part.gameObject.transform.forward + " Srf: " + surfaceNormal);
+					float dot = Vector3.Dot(surfaceNormal, thingThatLiftsParts.part.gameObject.transform.forward);
+					//print ("Dot: " + dot);
+					multiplier = 2.0f / (float)Math.Pow (0.3f * groundDistance + 1.0f, 2) * Math.Abs(dot) + 1.0f;
+				}
 			}
 
 			//print ("Lift Multiplier " + multiplier + " alt: " + vessel.radarAltitude);
-
 
 			//((thingThatAlsoLiftsPartsButMoves == null) ? thingThatLiftsParts.deflectionLiftCoeff : thingThatAlsoLiftsPartsButMoves.ctrlSurfaceArea) = nextLift;
 			thingThatLiftsParts.deflectionLiftCoeff = initialLift * multiplier;
