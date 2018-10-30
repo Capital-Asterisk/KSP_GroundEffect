d7f5bfa5cd875fb28b8594acfff293a092e3bb6d
Neal_Nicdao <chrisnicdao0@gmail.com>
2018-10-29 17:32:43 -0700

    FAR support
     * Added more comments
     * Added MagentaCarapace Spyware
     * A few other changes

--- GroundEffect/ModuleGroundEffect.cs
+++ GroundEffect/ModuleGroundEffect.cs
@@ -1,38 +1,86 @@
 ﻿using System;
+using System.Reflection;
 
 using UnityEngine;
 
 namespace KSP_GroundEffect
 {
+
+	// I don't know c#
+
 	[KSPAddon(KSPAddon.Startup.Flight, false)]
 	public class ModuleGroundEffect : PartModule 
 	{ 
 
-		const float ActivateAltitude = 400;
+		public const float ActivateAltitude = 400;
 
-		ModuleLiftingSurface thingThatLiftsParts;
-		ModuleControlSurface thingThatAlsoLiftsPartsButMoves;
-		float initialLift;
-		float initialLiftCtrl;
-		float wingSpan = 4.5f; // strange assumption m8
+		private float initialLift;
+		private float initialLiftCtrl;
+
+		// maybe do something with this later
+		private float wingSpan = 4.5f;
+
+		private ModuleLiftingSurface thingThatLiftsParts;
+		private ModuleControlSurface thingThatAlsoLiftsPartsButMoves;
+
+		//private bool farremLegacy;
+		private PartModule ferramModule;
+		private FieldInfo ferramField;
+
+		public static bool ferramEnabled = false;
 
 		public override void OnStart(StartState state) {
-			// Look through the list of part modules to find anything that inherits ModuleLiftingSurface
-			foreach (PartModule module in part.Modules) {
-				if (typeof(ModuleLiftingSurface).IsAssignableFrom(module.GetType())) {
-					thingThatLiftsParts = (ModuleLiftingSurface)(module);
-					initialLift = thingThatLiftsParts.deflectionLiftCoeff;
-					if (module is ModuleControlSurface) {
-						//print ("SURFAAAACE!");
-						thingThatAlsoLiftsPartsButMoves = (ModuleControlSurface)(module);
-						initialLiftCtrl = thingThatAlsoLiftsPartsButMoves.ctrlSurfaceArea;
-					}
-					print ("Ground effect module loaded");
+
+			if (ferramEnabled) {
+				// Do Ferrem stuff
+
+				// isn't legacy not even stilln't today but not used?
+				//if (part.Modules.Contains("FARControllableSurface")) {
+				//	ferramModule = part.Modules["FARControllableSurface"];
+				//} else if (part.Modules.Contains("FARWingAerodynamicModel")) {
+				//	ferramModule = part.Modules["FARWingAerodynamicModel"];
+				//}
+
+				if (part.Modules.Contains ("FARAeroPartModule")) {
+					ferramModule = part.Modules ["FARAeroPartModule"];
+					ferramField = ferramModule.GetType ().GetField ("worldSpaceAeroForce");
+				} else {
+					print ("FAR module not found");
+					Destroy (this);
 					return;
 				}
+
+				//foreach (PartModule pm in part.Modules) {
+				//	print ("module: " + pm.ClassName);
+				//}
+
+				//print ("farrem module: " + ferramModule);
+
+				print ("Ground effect module loaded with FAR");
+				return;
+
+			} else {
+				// Look through the list of part modules to find anything that inherits ModuleLiftingSurface
+				foreach (PartModule module in part.Modules) {
+					if (typeof(ModuleLiftingSurface).IsAssignableFrom(module.GetType())) {
+						thingThatLiftsParts = (ModuleLiftingSurface)(module);
+						initialLift = thingThatLiftsParts.deflectionLiftCoeff;
+						if (module is ModuleControlSurface) {
+							//print ("SURFAAAACE!");
+							thingThatAlsoLiftsPartsButMoves = (ModuleControlSurface)(module);
+							initialLiftCtrl = thingThatAlsoLiftsPartsButMoves.ctrlSurfaceArea;
+						}
+						print ("Ground effect module loaded");
+						return;
+					}
+				}
+				// This means there's a module manager error?
+
 			}
-			// This means there's a module manager error?
-			print("Ground effect module loaded for non-aero part?");
+
+			print ("Ground effect module loaded for non-aero part?");
+			Destroy (this);
+
 		}
 
 		public override void OnUpdate() {
@@ -40,19 +88,17 @@ namespace KSP_GroundEffect
 			float multiplier = 0;
 
 			// Checks to see if ground effect would have any significance
-			if (thingThatLiftsParts == null
-			    || ((vessel.situation & (Vessel.Situations.FLYING | Vessel.Situations.LANDED | Vessel.Situations.SPLASHED)) == 0)
+			if (((vessel.situation & (Vessel.Situations.FLYING | Vessel.Situations.LANDED | Vessel.Situations.SPLASHED)) == 0)
 			    || (vessel.radarAltitude > ActivateAltitude)
 			    || !vessel.mainBody.hasSolidSurface
 			    || !vessel.mainBody.atmosphere) {
-				// End immediately if it doesnt
-				//return;
 
-				// nevermind, just set multiplier to 1
-				// so that coefficients are set back to default values
+				// Set multiplier to 1 so that coefficients would have no change
 				multiplier = 1.0f;
 			} else {
-				
+
+				// 
+
 				float groundDistance = 0;
 				Vector3 surfaceNormal = vessel.gravityForPos.normalized;
 
@@ -70,27 +116,36 @@ namespace KSP_GroundEffect
 					// also set surface normal
 					surfaceNormal = ray.normal;
 				}
+
+				// By now, ground distance has been determined
+				// 0 means not close to terrain and no ocean
+
 				if (groundDistance != 0) {
-					//print ("Wing up: " + thingThatLiftsParts.part.gameObject.transform.forward + " Srf: " + surfaceNormal);
-					float dot = Vector3.Dot(surfaceNormal, thingThatLiftsParts.part.gameObject.transform.forward);
-					//print ("Dot: " + dot);
-					multiplier = 2.0f / (float)Math.Pow (0.3f * groundDistance + 1.0f, 2) * Math.Abs(dot) + 1.0f;
+					// Dot product with surface normal is how aligned the wing is to the ground.
+					// Vertical stabilizers would have a dot product of zero
+					// Horizontal wings will have 1
+					float dot = Math.Abs(Vector3.Dot(surfaceNormal, part.transform.forward));
+
+					multiplier = 2.0f / (float)Math.Pow (0.3f * groundDistance + 1.0f, 2) * dot + 1.0f;
 				}
 			}
+				
+			if (ferramEnabled) {
+				
+				// Since there's no values to adjust for FAR, just apply more lift force
+				Rigidbody rb = part.Rigidbody;
+				Vector3 worldSpaceAeroForce = (Vector3)(ferramField.GetValue (ferramModule));
+				rb.AddForce (worldSpaceAeroForce * (multiplier - 1.0f));
 
-			//print ("Lift Multiplier " + multiplier + " alt: " + vessel.radarAltitude);
+			} else {
 
-			//((thingThatAlsoLiftsPartsButMoves == null) ? thingThatLiftsParts.deflectionLiftCoeff : thingThatAlsoLiftsPartsButMoves.ctrlSurfaceArea) = nextLift;
-			thingThatLiftsParts.deflectionLiftCoeff = initialLift * multiplier;
-
-			if (thingThatAlsoLiftsPartsButMoves != null) {
-				//print ("CTRL SURFACE!!");
-				thingThatAlsoLiftsPartsButMoves.ctrlSurfaceArea = initialLiftCtrl * multiplier;
+				thingThatLiftsParts.deflectionLiftCoeff = initialLift * multiplier;
+				if (thingThatAlsoLiftsPartsButMoves != null) {
+					//print ("CTRL SURFACE!!");
+					thingThatAlsoLiftsPartsButMoves.ctrlSurfaceArea = initialLiftCtrl * multiplier;
+				}
 			}
-
 		}
-
 	}
-
 }
 
